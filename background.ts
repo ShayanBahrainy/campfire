ebc67b35b756df3421fa95a9b209448ec19bcd8c
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-12-24 21:03:23 -0800

    Add background with trees, and campfires (need work).

---
+++ background.ts
@@ -0,0 +1,67 @@
+import { Renderable, SubObject } from "./engine/renderable.js";
+import { Renderer } from "./engine/renderer.js";
+import { Shape } from "./engine/shape.js";
+
+export class Background implements Renderable {
+    priority: number;
+
+    shape: Shape | "none";
+    fillStyle: string;
+
+    renderparts: SubObject[];
+
+    x: number;
+    y: number;
+
+    constructor(private renderer: Renderer) {
+        this.renderer.addObject(this);
+        this.priority = 0;
+        this.shape = "none";
+        this.x = 0;
+        this.y = 0;
+    }
+
+    update() {
+        this.renderparts = [
+            //Ground + Sky
+            ...[
+                {shape: "rectangle" as Shape, x: 0, y: 0, priority: 0, fillStyle: "rgba(2, 2, 110, 1)", width: this.renderer.canvas.width, height: this.renderer.canvas.height * 2.0/3.0},
+                {shape: "rectangle" as Shape, x: 0, y: this.renderer.canvas.height * 2.0/3.0, priority: 0, fillStyle: "rgb(93, 52, 0)", width: this.renderer.canvas.width, height: this.renderer.canvas.height/3.0},
+            ]
+        ]
+
+        const y = this.renderer.canvas.height * 2.0/3.0
+
+        for (let i = 215; i < this.renderer.canvas.width; i += 400) {
+            let campfire: SubObject[] = [
+                //{x: i + 25, y: y - 25, apothem: 15, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 2, rotation: 105},
+                {x: i + 20, y: y - 20, apothem: 40, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 1, rotation: 90},
+                //{x: i + 20, y: y - 30, radius: 10, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 2, rotation: 0, angle: 180},
+
+
+                {x: i + 50, y: y, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(77, 52, 16, 1)", rotation: 135},
+                {x: i, y: y+5, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(89, 74, 53, 1)", rotation: 225},
+
+
+            ]
+
+            this.renderparts.push(...campfire)
+        }
+
+        for (let i = 15; i < this.renderer.canvas.width; i += 400) {
+            let tree: SubObject[] = [
+                {x: i - 20, y: y - 200, height: 200, width: 20, priority: 1, shape: "rectangle" as Shape, fillStyle: "rgb(93, 52, 0)"},
+                {x: i - 5, y: y - 240, apothem: 90, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
+                {x: i - 5, y: y - 180, apothem: 120, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
+                {x: i - 5, y: y - 100, apothem: 150, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90}
+            ]
+            this.renderparts.push(...tree)
+        }
+    }
+
+    collision(otherObject: Renderable): void {
+
+    }
+
+
+}
--- campfire.ts
+++ campfire.ts
@@ -3,15 +3,17 @@ import { Renderer } from "./engine/renderer.js"
 import { Shape } from "./engine/shape.js";
 
 import { Player } from "./player.js";
-//import { Background } from "./background.js";
+import { Background } from "./background.js";
 
 
 window.addEventListener("DOMContentLoaded", function () {
     let renderer: Renderer = new Renderer();
 
-    let player = new Player(0, 0, renderer);
+    //let player = new Player(0, 0, renderer);
 
-    renderer.addObject(player)
-    renderer.addObject({collision:function(){}, update:function(){}, x: 500, y: 500, shape:"rectangle", fillStyle:"rgb(255,255,255)", priority:0, text:"Hello World"  })
+    //renderer.addObject(player);
+    //renderer.addObject({collision:function(){}, update:function(){}, x: 500, y: 500, shape:"rectangle", fillStyle:"rgb(255,255,255)", priority:0, text:"Hello World"  });
+
+    renderer.addObject(new Background(renderer));
 
 })
