3080fb6026d8497c84ec3ba54ac3328f1212a090
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2026-01-05 21:56:18 -0800

    Rename renderable to baserenderable, and make object implement a specific type of BaseRenderable (e.g. PolygonRenderable)

--- engine/renderable.ts
+++ engine/renderable.ts
@@ -1,30 +1,40 @@
 import { Shape } from './shape.js';
 import { Point } from './point.js'
 
-export function isSubObject(obj: (SubObject | Renderable)): obj is SubObject {
+export function isSubObject(obj: (SubObject | BaseRenderable)): obj is SubObject {
   return "_parent" in obj;
 }
 
-export interface Renderable {
-    update(): void;
-    collision(otherObject: Renderable, subObject?: SubObject): void;
-    x: number;
-    y: number;
-    shape: Shape | "none";
-    priority: number;
-    fillStyle: string;
-    width?: number;
-    height?: number;
-    angle?: number;
-    rotation?: number;
-    radius?: number;
-    end?: Point;
-    renderparts?: SubObject[];
-    text?: string;
-    isdominant?: boolean;
-    apothem?: number;
-    vertexes?: number;
+export interface BaseRenderable {
+  update(): void;
+  collision(otherObject: BaseRenderable, subObject?: SubObject): void;
+
+  x: number;
+  y: number;
+
+  shape: Shape | "none";
+
+  priority: number;
+
+  fillStyle: string;
+
+  isdominant?: boolean;
+
+  text?: string;
+
+  renderparts?: SubObject[];
+
+  rotation?: number;
+
 }
+
+export type RectangleRenderable = (BaseRenderable & {shape: "rectangle", width: number, height: number});
+export type CircleRenderable = (BaseRenderable & {shape: "circle", radius: number, angle?: number});
+export type LineRenderable = (BaseRenderable & {shape: "line", end: Point, width: number});
+export type PolygonRenderable = (BaseRenderable & {shape: "polygon", apothem: number, vertexes: number});
+export type NoneRenderable = (BaseRenderable & {shape: "none"});
+
+
 
 export interface SubObject {
     x: number;
@@ -42,6 +52,6 @@ export interface SubObject {
     isdominant?: boolean;
     apothem?: number;
     vertexes?: number;
-    _parent?: Renderable;
+    _parent?: BaseRenderable;
     nocollide?: boolean;
 }
