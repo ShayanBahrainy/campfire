512358d84e3ec2f0eb24d5f6e208596ac3415d20
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2026-01-12 22:15:48 -0800

    Refactor so each chunk is a independent class, embers spawn in random quantities, and follow a sin wave movement upwards.

---
+++ chunk.ts
@@ -0,0 +1,126 @@
+import { BaseRenderable, NoneRenderable, SubObject } from "./engine/renderable.js";
+import { Renderer } from "./engine/renderer.js";
+import { Shape } from "./engine/shape.js";
+
+interface Ember extends SubObject {
+    vibration?: number;
+}
+
+export class Chunk implements NoneRenderable {
+    x: number;
+    y: number;
+
+    priority: number;
+
+    fillStyle: string;
+
+    shape: "none";
+
+    trees: SubObject[][];
+
+    campfires: SubObject[][];
+
+    embers: Ember[][];
+
+    renderer: Renderer;
+
+    renderparts?: SubObject[];
+
+    private generateChunk(x: number, y: number, width: number): void {
+        let generateFloat: (a: number, b: number) => number = this.renderer.generateFloat.bind(this.renderer);
+
+        for (let i = Math.floor(x / 400) * 400 + 175; i < width + x; i += 400) {
+            if (this.renderer.generateRandom(i, y) % 6 == 0) continue;
+
+            let campfire: SubObject[] = [
+                {x: i + 25, y: y - 20, apothem: 40, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 1, rotation: 90},
+                {x: i + 35, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(77, 52, 16, 1)", rotation: 135},
+                {x: i + 10, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(89, 74, 53, 1)", rotation: 225},
+
+            ];
+
+            //Generate embers
+            if (this.renderer.generateFloat(i, y) < 0.25) {
+                const ember_count = Math.floor(Math.random() * 6);
+
+                const v_signs: number[] = [];
+                for (let j = 0; j < ember_count; j++) v_signs.push(Math.random() > 0.5 ? 1 : -1);
+
+                let embers: Ember[] = [];
+
+                for (let j = 0; j < ember_count; j++){
+                    embers.push({x: i + generateFloat(i, y) * 10, y: y - 125 + Math.random() * 50, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2, vibration: generateFloat(i + j, y) * 5 * v_signs[j] } as Ember)
+                }
+
+                this.embers.push(
+                    embers
+                );
+            }
+
+            this.campfires.push(
+                campfire
+            );
+        }
+
+        for (let i = Math.floor(x / 400) * 400; i < width + x; i += 400) {
+            let tree: SubObject[] = [
+                {x: i - 20, y: y - 200, height: 200, width: 20, priority: 1, shape: "rectangle" as Shape, fillStyle: "rgb(93, 52, 0)"},
+                {x: i - 5, y: y - 240, apothem: 90, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
+                {x: i - 5, y: y - 180, apothem: 120, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
+                {x: i - 5, y: y - 100, apothem: 150, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90}
+            ]
+            this.trees.push(tree)
+        }
+    }
+
+
+    constructor(renderer: Renderer, x: number, y: number, width: number) {
+        renderer.addObject(this);
+        this.renderer = renderer;
+
+        this.campfires = [];
+        this.embers = [];
+        this.trees = [];
+
+        this.generateChunk(x, y, width);
+    }
+
+    update(): void {
+        this.renderparts = [];
+
+        for (const campfire of this.campfires) {
+            this.renderparts.push(...campfire);
+        }
+
+        for (const ember of this.embers) {
+            this.renderparts.push(...ember);
+        }
+
+        for (const tree of this.trees) {
+            this.renderparts.push(...tree);
+        }
+
+        for (const ember of this.embers) {
+            for (const subObject of ember) {
+                subObject.y -= 1;
+                subObject.x -= subObject.vibration * Math.cos(subObject.y / 10);
+            }
+        }
+    }
+
+
+    destruct(): void {
+        delete this.renderparts;
+        delete this.campfires;
+        delete this.embers;
+        delete this.trees;
+
+        this.renderer.removeObject(this);
+    }
+
+    collision(otherObject: BaseRenderable, subObject?: SubObject): void {
+
+    }
+
+
+}
--- map.ts
+++ map.ts
@@ -1,3 +1,4 @@
+import { Chunk } from "./chunk.js";
 import { BaseRenderable, NoneRenderable, SubObject } from "./engine/renderable.js";
 import { Renderer } from "./engine/renderer.js";
 import { Shape } from "./engine/shape.js";
@@ -14,12 +15,15 @@ export class MapMaker implements NoneRenderable {
 
     renderer: Renderer;
 
-    renderparts?: SubObject[];
-
     prevcx: number;
     prevcy: number;
 
+    chunks: Map<number, Map<number, Chunk>>;
+
     constructor(renderer: Renderer) {
+
+        this.chunks = new Map<number, Map<number, Chunk>>();
+
         renderer.addObject(this);
         this.renderer = renderer;
 
@@ -38,18 +42,28 @@ export class MapMaker implements NoneRenderable {
 
     private generateChunk(x: number, y: number, width: number): SubObject[] {
         const components: SubObject[] = []
-
-
+        let generateFloat: (a: number, b: number) => number = this.renderer.generateFloat.bind(this.renderer);
         for (let i = Math.floor(x / 400) * 400 + 175; i < width + x; i += 400) {
+            if (this.renderer.generateRandom(i, y) % 6 == 0) continue;
+
             let campfire: SubObject[] = [
                 {x: i + 25, y: y - 20, apothem: 40, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 1, rotation: 90},
-
-
                 {x: i + 35, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(77, 52, 16, 1)", rotation: 135},
                 {x: i + 10, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(89, 74, 53, 1)", rotation: 225},
 
+            ];
 
-            ]
+            if (this.renderer.generateFloat(i, y) > 0.25) {
+                let embers: SubObject[] = [
+                    {x: i + generateFloat(i, y) * 10, y: y - 125, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2},
+                    {x: i - generateFloat(i, y) * 10, y: y - 75, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2},
+                    {x: i + generateFloat(i, y) * 50, y: y - 75, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2}
+                ];
+
+                campfire.push(
+                    ...embers
+                );
+            }
 
             components.push(...campfire)
         }
@@ -68,12 +82,10 @@ export class MapMaker implements NoneRenderable {
     }
 
     update(): void {
-
         const h_chunk_factor = 800;
         const v_chunk_factor = 600;
 
         const follow_point = this.renderer.getFollowPoint();
-
 
         const cx = Math.floor(follow_point.x / h_chunk_factor);
         const cy = Math.floor(follow_point.y / v_chunk_factor);
@@ -82,17 +94,29 @@ export class MapMaker implements NoneRenderable {
             return;
         }
 
-        this.renderparts = [];
+        for (let x of this.chunks.keys()) {
+            for (let y of this.chunks.get(x).keys()) {
+                if (x > cx + 2 || x < cx - 2 || y > cy + 2 || y < cy + 2) {
+                    this.chunks.get(x).get(y).destruct();
+                    this.chunks.get(x).delete(y);
+                }
+            }
+        }
 
-        for (let i = cx - 3; i < cx + 3; i++) {
-            for (let j = cy - 3; j < cy + 3; j++) {
-                this.renderparts.push(...this.generateChunk(i * h_chunk_factor, j * v_chunk_factor - 200, h_chunk_factor));
+        for (let i = cx - 2; i < cx + 3; i++) {
+            for (let j = cy - 2; j < cy + 3; j++) {
+                if (!this.chunks.get(i)) {
+                    this.chunks.set(i, new Map<number, Chunk>());
+                }
+
+                if (!this.chunks.get(i).get(j)) {
+                    this.chunks.get(i).set(j, new Chunk(this.renderer, i * h_chunk_factor, j * v_chunk_factor - 200, h_chunk_factor));
+                }
             }
         }
 
         this.prevcx = cx;
         this.prevcy = cy;
-
     }
 
     collision(otherObject: BaseRenderable, subObject?: SubObject): void {
