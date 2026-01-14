f942300a95946f5b8028cd65f740ffb3d4680e16
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2026-01-13 21:19:33 -0800

    Delete unused and commented out code.

--- background.ts
+++ background.ts
@@ -28,37 +28,6 @@ export class Background implements NoneRenderable {
                 {shape: "rectangle" as Shape, x: 0, y: 0, priority: 0, fillStyle: "rgba(2, 2, 110, 1)", width: this.renderer.canvas.width, height: this.renderer.canvas.height, nocollide: true, screenpositioning: true},
             ]
         ]
-
-        /*
-
-
-        const follow_point = this.renderer.getFollowPoint();
-        const y = this.renderer.canvas.height * 2.0/3.0
-
-        for (let i = 215; i < this.renderer.canvas.width; i += 400) {
-            let campfire: SubObject[] = [
-                {x: i + 25, y: y - 20, apothem: 40, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 1, rotation: 90},
-
-
-                {x: i + 35, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(77, 52, 16, 1)", rotation: 135},
-                {x: i + 10, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(89, 74, 53, 1)", rotation: 225},
-
-
-            ]
-
-            this.renderparts.push(...campfire)
-        }
-
-        for (let i = 15; i < this.renderer.canvas.width; i += 400) {
-            let tree: SubObject[] = [
-                {x: i - 20, y: y - 200, height: 200, width: 20, priority: 1, shape: "rectangle" as Shape, fillStyle: "rgb(93, 52, 0)"},
-                {x: i - 5, y: y - 240, apothem: 90, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
-                {x: i - 5, y: y - 180, apothem: 120, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
-                {x: i - 5, y: y - 100, apothem: 150, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90}
-            ]
-            this.renderparts.push(...tree)
-        }
-        */
     }
 
     collision(otherObject: BaseRenderable): void {
--- map.ts
+++ map.ts
@@ -1,7 +1,6 @@
 import { Chunk } from "./chunk.js";
 import { BaseRenderable, NoneRenderable, SubObject } from "./engine/renderable.js";
 import { Renderer } from "./engine/renderer.js";
-import { Shape } from "./engine/shape.js";
 
 export class MapMaker implements NoneRenderable {
     x: number;
@@ -38,47 +37,6 @@ export class MapMaker implements NoneRenderable {
         this.priority = 2;
 
         this.fillStyle = "";
-    }
-
-    private generateChunk(x: number, y: number, width: number): SubObject[] {
-        const components: SubObject[] = []
-        let generateFloat: (a: number, b: number) => number = this.renderer.generateFloat.bind(this.renderer);
-        for (let i = Math.floor(x / 400) * 400 + 175; i < width + x; i += 400) {
-            if (this.renderer.generateRandom(i, y) % 6 == 0) continue;
-
-            let campfire: SubObject[] = [
-                {x: i + 25, y: y - 20, apothem: 40, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(250, 162, 0, 1)", priority: 1, rotation: 90},
-                {x: i + 35, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(77, 52, 16, 1)", rotation: 135},
-                {x: i + 10, y: y - 60, width: 10, height: 70, priority: 2, shape: "rectangle" as Shape, fillStyle: "rgba(89, 74, 53, 1)", rotation: 225},
-
-            ];
-
-            if (this.renderer.generateFloat(i, y) > 0.25) {
-                let embers: SubObject[] = [
-                    {x: i + generateFloat(i, y) * 10, y: y - 125, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2},
-                    {x: i - generateFloat(i, y) * 10, y: y - 75, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2},
-                    {x: i + generateFloat(i, y) * 50, y: y - 75, radius: 3, shape: "circle" as Shape, vertexes: 3, fillStyle: "rgba(231, 185, 100, 1)", angle: 360, priority: 2}
-                ];
-
-                campfire.push(
-                    ...embers
-                );
-            }
-
-            components.push(...campfire)
-        }
-
-        for (let i = Math.floor(x / 400) * 400; i < width + x; i += 400) {
-            let tree: SubObject[] = [
-                {x: i - 20, y: y - 200, height: 200, width: 20, priority: 1, shape: "rectangle" as Shape, fillStyle: "rgb(93, 52, 0)"},
-                {x: i - 5, y: y - 240, apothem: 90, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
-                {x: i - 5, y: y - 180, apothem: 120, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90},
-                {x: i - 5, y: y - 100, apothem: 150, shape: "polygon" as Shape, vertexes: 3, fillStyle: "rgba(23, 86, 23, 1)", priority: 2, rotation: 90}
-            ]
-            components.push(...tree)
-        }
-
-        return components;
     }
 
     update(): void {
