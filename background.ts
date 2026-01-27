a17002b59607c4b8fbf3614a34c65afee9cd86c6
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2026-01-26 21:49:36 -0800

    Optimize updating of background

--- background.ts
+++ background.ts
@@ -13,21 +13,39 @@ export class Background implements NoneRenderable {
     x: number;
     y: number;
 
+    prevWidth: number;
+    prevHeight: number;
+
     constructor(private renderer: Renderer) {
-        this.renderer.addObject(this);
         this.priority = 0;
         this.shape = "none";
         this.x = 0;
         this.y = 0;
-    }
-
-    update() {
         this.renderparts = [
             //Sky
             ...[
-                {shape: "rectangle" as Shape, x: 0, y: 0, priority: 0, fillStyle: "rgba(2, 2, 110, 1)", width: this.renderer.canvas.width, height: this.renderer.canvas.height, nocollide: true, screenpositioning: true},
+                {shape: "rectangle" as Shape, x: 0, y: 0, priority: -100, fillStyle: "rgba(2, 2, 110, 1)", width: this.renderer.canvas.width, height: this.renderer.canvas.height, nocollide: true, screenpositioning: true},
             ]
         ]
+        this.renderer.addObject(this);
+
+        this.prevWidth = this.renderer.canvas.width;
+        this.prevHeight = this.renderer.canvas.height;
+
+    }
+
+    update() {
+        if (this.renderer.canvas.width != this.prevWidth || this.renderer.canvas.height != this.prevHeight) {
+            this.renderparts = [
+                //Sky
+                ...[
+                    {shape: "rectangle" as Shape, x: 0, y: 0, priority: -100, fillStyle: "rgba(2, 2, 110, 1)", width: this.renderer.canvas.width, height: this.renderer.canvas.height, nocollide: true, screenpositioning: true},
+                ]
+            ]
+
+            this.prevHeight = this.renderer.canvas.height;
+            this.prevWidth = this.renderer.canvas.width;
+        }
     }
 
     collision(otherObject: BaseRenderable): void {
