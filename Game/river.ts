e4bc7e63a8d5a9527cee2ddca0f8dc712577b419
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2026-01-29 18:01:30 -0800

    Fix drawing of lines in River

--- Game/river.ts
+++ Game/river.ts
@@ -72,12 +72,14 @@ export class River implements NoneRenderable {
         for (let x = follow_point.x - MapMaker.HORIZONTAL_CHUNK_FACTOR * 2; x < follow_point.x + MapMaker.HORIZONTAL_CHUNK_FACTOR * 2; x += Math.abs(River.RIVER_SPEED * this.linetime)) {
             const start_point = new Point(x, this.line_y);
             const end_point = new Point(start_point.x + 200, this.line_y);
-            this.lines.push({x: start_point.x, y: start_point.y, end: end_point, shape: "line" as Shape, priority: 4, fillStyle: "rgb(255,255,255)"});
+            this.lines.push({x: start_point.x, y: start_point.y, end: end_point, shape: "line" as Shape, priority: 6, fillStyle: "rgb(255,255,255)"});
 
             if (this.line_y + this.delta_line_y > (cy + 100) + River.RIVER_HEIGHT || this.line_y + this.delta_line_y < (cy + 100) ) this.delta_line_y *= -1;
 
             this.line_y += this.delta_line_y;
         }
+
+        this.renderparts = [...this.lines, ...this.riverparts];
 
     }
 
@@ -112,7 +114,7 @@ export class River implements NoneRenderable {
         if (this.linetimer <= 0 && this.add_new_lines) {
             const start_point = new Point(cx + 1000, this.line_y);
             const end_point = new Point(start_point.x - 200, this.line_y);
-            this.lines.push({x: start_point.x, y: start_point.y, end: end_point, shape: "line" as Shape, priority: 4, fillStyle: "rgb(255,255,255)"});
+            this.lines.push({x: start_point.x, y: start_point.y, end: end_point, shape: "line" as Shape, priority: 6, fillStyle: "rgb(255,255,255)"});
 
             this.linetimer = this.linetime;
 
@@ -123,9 +125,9 @@ export class River implements NoneRenderable {
 
         this.linetimer -= 1;
 
-        for (let i = 0; i < this.lines.length; i++) {
+        for (let i = this.lines.length - 1; i > -1; i--) {
             const subObject = this.lines[i];
-            if (Math.abs(subObject.x - cx) > this.renderer.canvas.width) {
+            if (Math.abs(subObject.x - cx) > 2000) {
                 this.lines.splice(i, 1);
                 continue;
             }
